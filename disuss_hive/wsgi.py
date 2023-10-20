5942cf58c33c45035dcbe5d9125729041b793573
tanmaycodes13 <tanmaycodes@outlook.com>
2023-10-20 20:50:29 +0530

    Black formatter and adding requiremensts.txt file

--- disuss_hive/wsgi.py
+++ disuss_hive/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disuss_hive.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "disuss_hive.settings")
 
 application = get_wsgi_application()
